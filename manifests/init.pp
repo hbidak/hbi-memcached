0d675e1410df8e64e9483560472a6d6aade19267
hbidak <hbidak@gmail.com>
2018-10-09 04:14:13 -0700

    run memcached service + add local puppetlabs-stdlib project (external to this one)

--- manifests/init.pp
+++ manifests/init.pp
@@ -43,6 +43,18 @@
 # Copyright 2018 Your name here, unless otherwise noted.
 #
 class memcached {
-
-
+  package { 'memcached': ensure => installed, }
+  file { '/etc/memcached.conf':
+    source => 'puppet:///modules/memcached/memcached.conf',
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    require => Package['memcached'],
+  }
+  service { 'memcached':
+    ensure => running,
+    enable => true,
+    require => [ Package['memcached'],
+      File['/etc/memcached.conf']],
+  }
 }
